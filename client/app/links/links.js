7242c019d8fdd11ac555ef88e75419cc40493091
Dimitri Temkin <dimitri.temkin@gmail.com>
2017-03-12 04:00:19 +1100

    Add Links conroller

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,31 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+
+  $scope.getAll = function () {
+    Links.getAll()
+      .then(function (data) {
+        console.log('data - getAll', data);
+        $scope.data = data;
+        // $location.path('/links');
+      })
+      .catch(function (error) {
+        console.error('ERROR ----->>', error);
+      });
+  };
+
+  // $scope.addOne = function () {
+    Links.addOne($scope.data)
+      .then(function (data) {
+        console.log('data - LinksController - addOne', data);
+        $scope.data = data;
+        // $location.path('/links');
+      })
+      .catch(function (error) {
+        console.error('ERROR ----->>', error);
+      });
+  // };
+
+  $scope.getAll();
 });
